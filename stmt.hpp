4e16ea03f5366a69a620684e0081031a7a21b2ff
doug@stream <doug@totem9.net>
2020-06-26 18:33:17 +0100

    simple additions to print function

--- stmt.hpp
+++ stmt.hpp
@@ -6,6 +6,8 @@ using namespace std;
 struct Stmt {
 	string type;
 
+	vector<string> literals; // temp
+
 	int build(InputFile& inp) {
 		printf("%02d: stmt: %s\n", inp.linepos+1, inp.peek().c_str());
 		if (inp.peeklower() == "print") do_print(inp);
@@ -15,9 +17,17 @@ struct Stmt {
 
 private:
 	void do_print(InputFile& inp) {
+		printf("parsing print...\n");
 		type = "print";
 		inp.next();
-		while (inp.is_strlit()) inp.next();
+		//while (inp.is_strlit()) inp.next();
+		while (true)
+			if (inp.is_strlit()) literals.push_back(inp.peek()), inp.next();
+			else break;
+		if (!inp.eol()) inp.die();
 		inp.nextline();
+
+		for (auto l : literals)
+			printf("  literal: %s\n", l.c_str());
 	}
 };
